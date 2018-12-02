406f22a61b5b86dfbb5f4a308c31346173db85a7
David Dimperio <daviddimperio@Davids-MacBook-Pro.local>
2018-12-02 14:21:28 -0600

    Add Signup Page

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,28 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Account
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">Login</a>
+          <a class="dropdown-item" href="#">Logout</a>
+          <a class="dropdown-item" href="/#/signup">Signup</a>
+        </div>
+      </li>
+    </ul>
+  </div>
+</nav>
     <router-view/>
   </div>
 </template>
